bdd641277c2b681d1ff0bf1f5bdc3f00f459a2dc
samettinho <77895683+samettinho@users.noreply.github.com>
2021-01-23 21:08:54 +0300

    Add files via upload

---
+++ Dictonary/Dictonary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> takimlar = new MyDictionary<string>();
+            takimlar.Add("Fenerbahce");
+            takimlar.Add("Galatasaray");
+            takimlar.Add("Besiktas");
+            Console.WriteLine(takimlar.Count);
+        }
+    }
+    class MyDictionary <T>
+    {
+        T[] array;
+        T[] temparray;
+
+        public MyDictionary()
+        {
+            array = new T[0];
+
+        }
+        public void Add(T item)
+        {
+            temparray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                array[i] = temparray[i];
+            }
+            array[array.Length - 1] = item; 
+        }
+        public int Count
+        {
+            get { return array.Length; }
+        }
+    }
+    }
